26bfb66ddcbe5fd7b1ad4c1fb38dd8a04cc19983
cjp-engr <67772382+cjp-engr@users.noreply.github.com>
2022-01-12 20:51:56 -0800

    handling error gracefully

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -111,7 +111,24 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: const Text('An error occured'),
+                  content: const Text('Something went wrong'),
+                  // ignore: deprecated_member_use
+                  actions: <Widget>[
+                    // ignore: deprecated_member_use
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                      child: const Text('Okay'),
+                    ),
+                  ],
+                ));
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
